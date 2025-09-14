57f93906cb8a71af37a5613696ae57209e14d5a8
KilowattSynthesis <180148956+KilowattSynthesis@users.noreply.github.com>
2025-09-14 14:58:03 -0600

    create MVP design

--- cad/conveyor_assembly_jig.py
+++ cad/conveyor_assembly_jig.py
@@ -1,7 +1,9 @@
+import itertools
 from dataclasses import dataclass
 from pathlib import Path
 
 import build123d as bd
+import build123d_ease as bde
 from build123d_ease import show
 from loguru import logger
 
@@ -10,24 +12,72 @@ from loguru import logger
 class Spec:
     """Specification for conveyor_assembly_jig."""
 
-    conveyor_assembly_jig_radius: float = 20
+    magnet_d: float = 2.1
+    magnet_h: float = 1.2
+
+    # MARK: Braille specs.
+    dot_pitch_x: float = 2.5
+    dot_pitch_y: float = 2.5
+    cell_pitch_x: float = 6.0
+    cell_pitch_y: float = 10.0
+    cell_count_x: int = 4
+    cell_count_y: int = 2
+
+    # MARK: Body specs.
+    total_z: float = 5.0
 
     def __post_init__(self) -> None:
         """Post initialization checks."""
 
+    @property
+    def total_x(self) -> float:
+        """Total X dimension."""
+        return (self.cell_count_x * self.cell_pitch_x) + (2 * 5.0)
 
-def make_conveyor_assembly_jig(spec: Spec) -> bd.Part | bd.Compound:
+    @property
+    def total_y(self) -> float:
+        """Total Y dimension."""
+        return (self.cell_count_y * self.cell_pitch_y) + (2 * 5.0)
+
+
+# MARK: conveyor_assembly_jig
+def conveyor_assembly_jig(spec: Spec) -> bd.Part | bd.Compound:
     """Create a CAD model of conveyor_assembly_jig."""
     p = bd.Part(None)
 
-    p += bd.Cylinder(radius=spec.conveyor_assembly_jig_radius, height=20)
+    p += bd.Box(
+        spec.total_x, spec.total_y, spec.total_z, align=bde.align.ANCHOR_BOTTOM
+    )
+
+    # Remove the grid of magnets.
+    for cell_center_x, cell_center_y in itertools.product(
+        bde.evenly_space_with_center(
+            count=spec.cell_count_x, spacing=spec.cell_pitch_x
+        ),
+        bde.evenly_space_with_center(
+            count=spec.cell_count_y, spacing=spec.cell_pitch_y
+        ),
+    ):
+        for dot_x, dot_y in itertools.product(
+            bde.evenly_space_with_center(
+                count=2, spacing=spec.dot_pitch_x, center=cell_center_x
+            ),
+            bde.evenly_space_with_center(
+                count=3, spacing=spec.dot_pitch_y, center=cell_center_y
+            ),
+        ):
+            p -= bd.Cylinder(
+                spec.magnet_d / 2,
+                spec.magnet_h,
+                align=bde.align.ANCHOR_TOP,
+            ).translate((dot_x, dot_y, spec.total_z))
 
     return p
 
 
 if __name__ == "__main__":
     parts = {
-        "conveyor_assembly_jig": show(make_conveyor_assembly_jig(Spec())),
+        "conveyor_assembly_jig": show(conveyor_assembly_jig(Spec())),
     }
 
     logger.info("Showing CAD model(s)")
